8af611ea8c5bef025a21ac0dbf46096134be7f09
MaulingMonkey <git@maulingmonkey.com>
2017-03-27 18:16:30 -0700

    Start working on support for Microsoft's Android tooling.
    - Add new solution platform, "Android (ARM, Microsoft)"
    - Add new solution folder, "Projects (Android, Microsoft)"
    - Add new project, mmkAndroidTest.  This is a placeholder for the Java portion.

---
+++ mmkAndroidTest/src/com/mmkAndroidTest/mmkAndroidTest.java
@@ -0,0 +1,56 @@
+/* Copyright 2017 MaulingMonkey
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+package com.mmkAndroidTest;
+
+import android.app.Activity;
+import android.text.method.ScrollingMovementMethod;
+import android.os.Bundle;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+public class mmkAndroidTest extends Activity {
+	static {
+		//System.loadLibrary("mmkAndroidTest");
+	}
+
+	private ScrollView sv;
+	private TextView tv;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		//mmkDebugStackSearchPaths(getApplicationInfo().nativeLibraryDir);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("mmkAndroidTest Placeholder");
+		//sb.append(traceDemo());
+		//sb.append("\n\n");
+		//sb.append(inspectElf(getApplicationInfo().nativeLibraryDir, System.mapLibraryName("mmkNvidiaCodeworksTest")));
+
+		tv = new TextView(this);
+		tv.setText(sb.toString());
+
+		sv = new ScrollView(this);
+		sv.addView(tv);
+		setContentView(sv);
+	}
+
+	// TODO: Implement
+	// public native void mmkDebugStackSearchPaths(String elfDir);
+	// public native String traceDemo();
+	// public native String inspectElf(String elfDir, String elfName);
+}
